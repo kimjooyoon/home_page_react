c24e6650868b904d396c42e69e060eafd59d28bf
kimjooyoon <asketeddy@gmail.com>
2024-07-09 04:35:20 +0900

    feat(storybook): add menuitem

--- taskbox/src/stories/atoms/dropdown/DropDownAtom.stories.tsx
+++ taskbox/src/stories/atoms/dropdown/DropDownAtom.stories.tsx
@@ -1,6 +1,7 @@
 import {Meta, StoryObj} from '@storybook/react';
 
 import DropDown from '../../../../../src/components/atoms/dropdown/DropDownAtom';
+import MenuItemAtom from '../../../../../src/components/atoms/dropdown/menuitem/MenuItemAtom';
 
 const meta: Meta<typeof DropDown> = {
   component: DropDown,
@@ -12,5 +13,6 @@ type Story = StoryObj<typeof DropDown>;
 
 export const Basic: Story = {
   args: {
+    children: <MenuItemAtom value={'test'} name={'Test'}/>
   }
 };
