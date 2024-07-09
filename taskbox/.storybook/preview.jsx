db5178e80165651a308856ac1a363c59d187c1ff
kimjooyoon <asketeddy@gmail.com>
2024-07-09 17:56:54 +0900

    feat(storybook): add viewport addon

--- taskbox/.storybook/preview.jsx
+++ taskbox/.storybook/preview.jsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from '@mui/material/styles';
 import {withThemeFromJSXProvider} from '@storybook/addon-themes';
-
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import lightTheme from '../../src/styles/lightTheme';
 import darkTheme from '../../src/styles/darkTheme';
 import {GlobalStyles} from "@mui/material";
@@ -15,3 +15,25 @@ export const decorators = [
         Provider: ThemeProvider,
         GlobalStyles,
     })];
+
+export const parameters = {
+    viewport: {
+        viewports: {
+            ...INITIAL_VIEWPORTS,
+            small: {
+                name: 'Small',
+                styles: {
+                    width: '500px',
+                    height: '963px',
+                },
+            },
+            responsive: {
+                name: 'Responsive',
+                styles: {
+                    width: '100%',
+                    height: '100%',
+                },
+            },
+        },
+    },
+}
