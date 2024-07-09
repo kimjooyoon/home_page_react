8e6287d34aa4d3a9acb3de1887491fbcf1e2c6f0
kimjooyoon <asketeddy@gmail.com>
2024-07-09 17:42:17 +0900

    feat(atoms): add toolbar navigation

--- src/components/atoms/appbar/AppBarAtom.tsx
+++ src/components/atoms/appbar/AppBarAtom.tsx
@@ -12,7 +12,7 @@ import BoxAtom from "../box/BoxAtom.tsx";
 import TypographyAtom from "../typography/TypographyAtom.tsx";
 import MenuItemAtom from "../menuitem/MenuItemAtom.tsx";
 import {IconAtomMenu} from "../icon/IconAtom.tsx";
-import ToolBarAtom from "../toolbar/ToolBarAtom.tsx";
+import {ToolBarAtomNavigation} from "../toolbar/ToolBarAtom.tsx";
 import DividerAtom from "../divider/DividerAtom.tsx";
 import ContainerAtom from "../container/ContainerAtom.tsx";
 import DrawerAtom from "../drawer/DrawerAtom.tsx";
@@ -73,28 +73,7 @@ function AppAppBar({mode}: AppAppBarProps) {
         }}
       >
         <ContainerAtom maxWidth="lg">
-          <ToolBarAtom
-            variant="regular"
-            sx={(theme) => ({
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              flexShrink: 0,
-              borderRadius: '999px',
-              bgcolor:
-                theme.palette.mode === 'light'
-                  ? 'rgba(255, 255, 255, 0.4)'
-                  : 'rgba(0, 0, 0, 0.4)',
-              backdropFilter: 'blur(24px)',
-              maxHeight: 40,
-              border: '1px solid',
-              borderColor: 'divider',
-              boxShadow:
-                theme.palette.mode === 'light'
-                  ? `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`
-                  : '0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)',
-            })}
-          >
+          <ToolBarAtomNavigation>
             <BoxAtom
               sx={{
                 flexGrow: 1,
@@ -252,7 +231,7 @@ function AppAppBar({mode}: AppAppBarProps) {
                 </BoxAtom>
               </DrawerAtom>
             </BoxAtom>
-          </ToolBarAtom>
+          </ToolBarAtomNavigation>
         </ContainerAtom>
       </AppBar>
     </div>
