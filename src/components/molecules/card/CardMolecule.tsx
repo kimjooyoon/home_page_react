621610cb070a822923094e341c1dd490a13e970b
kimjooyoon <asketeddy@gmail.com>
2024-07-09 15:26:32 +0900

    fix(molecules): modify card

--- src/components/molecules/card/CardMolecule.tsx
+++ src/components/molecules/card/CardMolecule.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {Box, Card, CardContent, Typography} from "@mui/material";
+import {Box, Card, CardContent} from "@mui/material";
+import TypographyAtom from "../../atoms/typography/TypographyAtom.tsx";
 
 export interface Props {
   title: string,
@@ -11,39 +12,40 @@ export interface Props {
   description: string,
 }
 
-const CardMolecule: React.FC<Props> = ({
-                                         title, name, email,
-                                         company, phone,
-                                         position, description,
-                                       }) => {
-  const card = (
-    <React.Fragment>
-      <CardContent>
-        <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
-          {title}
-        </Typography>
-        <Typography variant="h5" component="div">
-          {name}
-        </Typography>
-        <br/>
-        <Typography sx={{mb: 1.5}} color="text.secondary">
-          Email: {email}
-          <br/>Position: {position}
-          <br/>Phone: {phone}
-          <br/>Company: {company}
-        </Typography>
-        <Typography variant="body2">
-          {description}
-        </Typography>
-      </CardContent>
-    </React.Fragment>
-  );
+const CardMolecule: React.FC<Props> =
+  ({
+     title, name, email,
+     company, phone,
+     position, description,
+   }) => {
+    const card = (
+      <React.Fragment>
+        <CardContent>
+          <TypographyAtom sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+            {title}
+          </TypographyAtom>
+          <TypographyAtom variant="h5" component="div">
+            {name}
+          </TypographyAtom>
+          <br/>
+          <TypographyAtom sx={{mb: 1.5}} color="text.secondary">
+            Email: {email}
+            <br/>Position: {position}
+            <br/>Phone: {phone}
+            <br/>Company: {company}
+          </TypographyAtom>
+          <TypographyAtom variant="body2">
+            {description}
+          </TypographyAtom>
+        </CardContent>
+      </React.Fragment>
+    );
 
-  return <div>
-    <Box sx={{minWidth: 275}}>
-      <Card variant="outlined" role={'article'} data-testid="text-form-card">{card}</Card>
-    </Box>
-  </div>;
-}
+    return <div>
+      <Box sx={{minWidth: 275}}>
+        <Card variant="outlined" role={'article'} data-testid="text-form-card">{card}</Card>
+      </Box>
+    </div>;
+  }
 
 export default CardMolecule;
