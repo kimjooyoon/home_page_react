38ca5c28bbe60243e9fb7529af01eafcdffce14f
kimjooyoon <asketeddy@gmail.com>
2024-07-24 16:42:18 +0900

    feat(atom): add grid

---
+++ src/components/atoms/grid/GridAtom.tsx
@@ -0,0 +1,8 @@
+import {GridProps} from "@mui/material";
+import Grid from "@mui/material/Grid";
+
+const GridAtom = (props: GridProps) => {
+  return <Grid {...props} />
+}
+
+export default GridAtom;
